5053af207ba5ced5a8b1f738f7fd9d87d4560156
Franco Vuoto <franco.vuoto@eng.it>
2025-01-07 16:58:02 +0100

    rm unuseful javadoc

--- src/main/java/it/gov/pagopa/pu/workflow/controller/IngestionFlowControllerImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/controller/IngestionFlowControllerImpl.java
@@ -8,14 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * WorkflowHubApiImpl class
- * Implements the methods of the WorkflowHubApi interface
- * This class is used to create a Payment Ingestion Workflow
- * by its ingestionFileId and return the workflowId
- *
- * @see IngestionFlowApi
- */
 @Slf4j
 @RestController
 public class IngestionFlowControllerImpl implements IngestionFlowApi {
@@ -26,10 +18,6 @@ public class IngestionFlowControllerImpl implements IngestionFlowApi {
         this.paymentsReportingIngestionWFClient = paymentsReportingIngestionWFClient;
     }
 
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public ResponseEntity<WorkflowCreatedDTO> ingestPaymentsReportingFile(@PathVariable("ingestionFlowFileId") Long ingestionFlowFileId) {
         log.info("Creating Payments Reporting Ingestion Workflow for ingestionFlowFileId: {}", ingestionFlowFileId);
