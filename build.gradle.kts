9226aa1069fa38bda21907f55c6f8eaa239eb8e9
antonio.torre <antonio.torre@pagopa.it>
2025-01-12 17:45:42 +0100

    P4ADEV-1757 NotRetryableActivityException handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ val openApiToolsVersion = "0.2.6"
 val micrometerVersion = "1.4.1"
 val temporalVersion = "1.27.0"
 val protobufJavaVersion = "3.25.5"
-val activitiesVersion = "1.23.1"
+val activitiesVersion = "1.30.1"
 val bouncycastleVersion = "1.79"
 
 dependencies {
@@ -50,6 +50,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
+  implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
@@ -110,7 +111,16 @@ configurations {
 }
 
 tasks.compileJava {
-  dependsOn("openApiGenerate")
+  dependsOn("dependenciesBuild")
+}
+
+tasks.register("dependenciesBuild") {
+  group = "AutomaticallyGeneratedCode"
+  description = "grouping all together automatically generate code tasks"
+
+  dependsOn(
+    "openApiGenerate"
+  )
 }
 
 configure<SourceSetContainer> {
@@ -136,9 +146,10 @@ openApiGenerate {
       "useSpringBoot3" to "true",
       "interfaceOnly" to "true",
       "useTags" to "true",
-      "generateConstructorWithAllArgs" to "false",
+      "useBeanValidation" to "true",
+      "generateConstructorWithAllArgs" to "true",
       "generatedConstructorWithRequiredArgs" to "true",
-      "additionalModelTypeAnnotations" to "@lombok.Data @lombok.Builder @lombok.AllArgsConstructor"
+      "additionalModelTypeAnnotations" to "@lombok.Builder"
     )
   )
 }
--- src/main/java/it/gov/pagopa/pu/workflow/WorkflowApplication.java
+++ src/main/java/it/gov/pagopa/pu/workflow/WorkflowApplication.java
@@ -1,9 +1,12 @@
 package it.gov.pagopa.pu.workflow;
 
+import it.gov.pagopa.payhub.activities.aspect.NotRetryableActivityExceptionHandlerAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(NotRetryableActivityExceptionHandlerAspect.class)
 public class WorkflowApplication {
 
 	public static void main(String[] args) {
