b4a749a4aee18559574756a48277f95496ba6bea
macacia <marco.cacia@eng.it>
2025-01-15 11:53:13 +0100

    P4ADEV-1587 added TransferClassificationWF

---
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/classifications/transfer/TransferClassificationWFClient.java
@@ -0,0 +1,29 @@
+package it.gov.pagopa.pu.workflow.wf.classifications.transfer;
+
+import io.temporal.client.WorkflowClient;
+import it.gov.pagopa.pu.workflow.service.WorkflowService;
+import it.gov.pagopa.pu.workflow.wf.ingestionflow.treasury.opi.wfingestion.TreasuryOpiIngestionWF;
+import it.gov.pagopa.pu.workflow.wf.ingestionflow.treasury.opi.wfingestion.TreasuryOpiIngestionWFImpl;
+import org.springframework.stereotype.Service;
+
+import static it.gov.pagopa.pu.workflow.utilities.Utilities.generateWorkflowId;
+
+@Service
+public class TransferClassificationWFClient {
+
+  private final WorkflowService workflowService;
+
+  public TransferClassificationWFClient(WorkflowService workflowService) {
+    this.workflowService = workflowService;
+  }
+
+  public String ingest(Long ingestionFlowFileId) {
+    String workflowId = generateWorkflowId(ingestionFlowFileId, TreasuryOpiIngestionWFImpl.TASK_QUEUE);
+    TreasuryOpiIngestionWF workflow = workflowService.buildWorkflowStub(
+      TreasuryOpiIngestionWF.class,
+      TreasuryOpiIngestionWFImpl.TASK_QUEUE,
+      workflowId);
+    WorkflowClient.start(workflow::ingest, ingestionFlowFileId);
+    return workflowId;
+  }
+}
