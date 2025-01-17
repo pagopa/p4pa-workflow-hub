f2fa2c74a3a9f93850e1024685995cf7c0be3ff5
Franco Vuoto <franco.vuoto@eng.it>
2025-01-17 10:50:54 +0100

    add further test

---
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/treasury/opi/activity/StartIufClassificationActivityTest.java
@@ -0,0 +1,46 @@
+package it.gov.pagopa.pu.workflow.wf.ingestionflow.treasury.opi.activity;
+
+import it.gov.pagopa.pu.workflow.wf.classification.iuf.IufClassificationWFClient;
+import it.gov.pagopa.pu.workflow.wf.classification.iuf.dto.IufClassificationNotifyTreasurySignalDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+class StartIufClassificationActivityTest {
+
+    @Mock
+    private IufClassificationWFClient iufClassificationWFClientMock;
+
+    private StartIufClassificationActivityImpl startIufClassificationActivity;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startIufClassificationActivity = new StartIufClassificationActivityImpl(iufClassificationWFClientMock);
+    }
+
+    @Test
+    void testSignalIufClassificationWithStart() {
+        // Given
+        Long organizationId = 1L;
+        String iuf = "iuf-123";
+        String treasuryId = "treasury-456";
+
+        // When
+        startIufClassificationActivity.signalIufClassificationWithStart(organizationId, iuf, treasuryId);
+
+        // Then
+        ArgumentCaptor<IufClassificationNotifyTreasurySignalDTO> captor = ArgumentCaptor.forClass(IufClassificationNotifyTreasurySignalDTO.class);
+        verify(iufClassificationWFClientMock).notifyTreasury(captor.capture());
+
+        IufClassificationNotifyTreasurySignalDTO capturedDTO = captor.getValue();
+        assertEquals(organizationId, capturedDTO.getOrganizationId());
+        assertEquals(iuf, capturedDTO.getIuf());
+        assertEquals(treasuryId, capturedDTO.getTreasuryId());
+    }
+}
