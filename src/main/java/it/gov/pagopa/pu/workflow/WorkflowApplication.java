ea603d792c420d4ac649af8be9fd617231678295
antonio.torre <antonio.torre@pagopa.it>
2025-02-03 13:16:17 +0100

    fix datetime deserialization

--- src/main/java/it/gov/pagopa/pu/workflow/WorkflowApplication.java
+++ src/main/java/it/gov/pagopa/pu/workflow/WorkflowApplication.java
@@ -1,13 +1,19 @@
 package it.gov.pagopa.pu.workflow;
 
 import it.gov.pagopa.payhub.activities.aspect.NotRetryableActivityExceptionHandlerAspect;
+import it.gov.pagopa.payhub.activities.config.JsonConfig;
+import it.gov.pagopa.payhub.activities.config.RestTemplateConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
-@Import(NotRetryableActivityExceptionHandlerAspect.class)
+@Import({
+  JsonConfig.class,
+  RestTemplateConfig.class,
+  NotRetryableActivityExceptionHandlerAspect.class
+})
 public class WorkflowApplication {
 
 	public static void main(String[] args) {
