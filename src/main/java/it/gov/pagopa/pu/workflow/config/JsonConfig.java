6b29572113f04551a2aaa47523e2a35d9607ce1b
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-02-03 13:25:43 +0100

    fix: Fix datetime deserialization (#36)

---
+++ src/main/java/it/gov/pagopa/pu/workflow/config/JsonConfig.java
@@ -0,0 +1,7 @@
+package it.gov.pagopa.pu.workflow.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JsonConfig extends it.gov.pagopa.payhub.activities.config.JsonConfig {
+}
