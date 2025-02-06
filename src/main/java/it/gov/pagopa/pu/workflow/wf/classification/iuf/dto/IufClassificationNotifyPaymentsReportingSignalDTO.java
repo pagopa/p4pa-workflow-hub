5a745ea93fd8fd2c5ad99dd1aeee33ce79774537
macacia <marco.cacia@eng.it>
2025-02-06 10:24:30 +0100

    fix: Adding lombok NoArgsConstructor to DTOs (#41)

--- src/main/java/it/gov/pagopa/pu/workflow/wf/classification/iuf/dto/IufClassificationNotifyPaymentsReportingSignalDTO.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/classification/iuf/dto/IufClassificationNotifyPaymentsReportingSignalDTO.java
@@ -5,12 +5,14 @@ import it.gov.pagopa.payhub.activities.dto.classifications.PaymentsReportingTran
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class IufClassificationNotifyPaymentsReportingSignalDTO {
   // common fields
   private Long organizationId;
