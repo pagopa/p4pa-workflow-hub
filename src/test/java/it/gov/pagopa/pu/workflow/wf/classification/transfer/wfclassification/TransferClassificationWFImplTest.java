457d7c00d2e47a3a38675b1981c4d1a160134eea
antonio.torre <antonio.torre@pagopa.it>
2025-02-10 10:14:32 +0100

    fix: IngestionFlowFile codError renamed into errorDescription

--- src/test/java/it/gov/pagopa/pu/workflow/wf/classification/transfer/wfclassification/TransferClassificationWFImplTest.java
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/classification/transfer/wfclassification/TransferClassificationWFImplTest.java
@@ -1,20 +1,29 @@
 package it.gov.pagopa.pu.workflow.wf.classification.transfer.wfclassification;
 
+import io.temporal.workflow.Workflow;
+import it.gov.pagopa.payhub.activities.activity.classifications.TransferClassificationActivity;
+import it.gov.pagopa.payhub.activities.dto.classifications.TransferSemanticKeyDTO;
 import it.gov.pagopa.pu.workflow.wf.classification.transfer.config.TransferClassificationWfConfig;
 import it.gov.pagopa.pu.workflow.wf.classification.transfer.dto.TransferClassificationStartSignalDTO;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.mockito.Mockito.*;
+import java.util.function.Supplier;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class TransferClassificationWFImplTest {
-  private static final Long ORGANIZATION = 123L;
-  private static final String IUV = "01011112222333345";
-  private static final String IUR = "IUR";
-  private static final int TRANSFER_INDEX = 1;
+
+  @Mock
+  private TransferClassificationActivity transferClassificationActivityMock;
 
   private TransferClassificationWFImpl wf;
 
@@ -25,13 +34,43 @@ class TransferClassificationWFImplTest {
 
     when(applicationContextMock.getBean(TransferClassificationWfConfig.class))
       .thenReturn(transferClassificationWfConfig);
+    when(transferClassificationWfConfig.buildTransferClassificationActivityStub())
+      .thenReturn(transferClassificationActivityMock);
 
     wf = new TransferClassificationWFImpl();
     wf.setApplicationContext(applicationContextMock);
   }
 
+  @AfterEach
+  void verifyNoMoreInteractions(){
+    Mockito.verifyNoMoreInteractions(
+      transferClassificationActivityMock
+    );
+  }
+
   @Test
-  void givenClassifyThenSuccess() {
-    assertDoesNotThrow(() -> wf.startTransferClassification(new TransferClassificationStartSignalDTO(ORGANIZATION, IUV, IUR, TRANSFER_INDEX)));
+  void testSignalAndWfExecution() {
+    signalTransferClassification(1L, "iuv1", "iur1", 1);
+    signalTransferClassification(2L, "iuv1", "iur1", 1);
+    signalTransferClassification(2L, "iuv2", "iur2", 2);
+
+    try(MockedStatic<Workflow> workflowMock = Mockito.mockStatic(Workflow.class)) {
+      workflowMock.when(Workflow::isEveryHandlerFinished).thenReturn(true);
+
+      wf.classify();
+
+      workflowMock.verify(() -> Workflow.await(Mockito.argThat(Supplier::get)));
+
+      Mockito.verify(transferClassificationActivityMock)
+        .classify(new TransferSemanticKeyDTO(1L, "iuv1", "iur1", 1));
+      Mockito.verify(transferClassificationActivityMock)
+        .classify(new TransferSemanticKeyDTO(2L, "iuv1", "iur1", 1));
+      Mockito.verify(transferClassificationActivityMock)
+        .classify(new TransferSemanticKeyDTO(2L, "iuv2", "iur2", 2));
+    }
+  }
+
+  private void signalTransferClassification(Long organizationId, String iuv, String iur, int transferIndex) {
+    wf.startTransferClassification(new TransferClassificationStartSignalDTO(organizationId, iuv, iur, transferIndex));
   }
 }
