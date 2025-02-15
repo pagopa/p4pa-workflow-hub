1d4751e5e70b3cd914dd232c1f8e3e99415eb24d
antonio.torre <antonio.torre@pagopa.it>
2025-02-15 18:55:57 +0100

    feat: Performance logging

--- src/main/java/it/gov/pagopa/pu/workflow/connector/organization/config/OrganizationApisHolder.java
+++ src/main/java/it/gov/pagopa/pu/workflow/connector/organization/config/OrganizationApisHolder.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.pu.workflow.connector.organization.config;
 
+import it.gov.pagopa.payhub.activities.config.RestTemplateConfig;
 import it.gov.pagopa.pu.organization.client.generated.BrokerSearchControllerApi;
 import it.gov.pagopa.pu.organization.client.generated.OrganizationEntityControllerApi;
 import it.gov.pagopa.pu.organization.client.generated.OrganizationSearchControllerApi;
@@ -7,7 +8,6 @@ import it.gov.pagopa.pu.organization.client.generated.TaxonomySearchControllerAp
 import it.gov.pagopa.pu.organization.generated.ApiClient;
 import it.gov.pagopa.pu.organization.generated.BaseApi;
 import jakarta.annotation.PreDestroy;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -26,13 +26,18 @@ public class OrganizationApisHolder {
   private final ThreadLocal<String> bearerTokenHolder = new ThreadLocal<>();
 
   public OrganizationApisHolder(
-    @Value("${rest.organization.base-url}") String baseUrl,
-
-    RestTemplateBuilder restTemplateBuilder) {
+    OrganizationClientConfig clientConfig,
+    RestTemplateBuilder restTemplateBuilder
+  ) {
     RestTemplate restTemplate = restTemplateBuilder.build();
     ApiClient apiClient = new ApiClient(restTemplate);
-    apiClient.setBasePath(baseUrl);
+    apiClient.setBasePath(clientConfig.getBaseUrl());
     apiClient.setBearerToken(bearerTokenHolder::get);
+    apiClient.setMaxAttemptsForRetry(Math.max(1, clientConfig.getMaxAttempts()));
+    apiClient.setWaitTimeMillis(clientConfig.getWaitTimeMillis());
+    if (clientConfig.isPrintBodyWhenError()) {
+      restTemplate.setErrorHandler(RestTemplateConfig.bodyPrinterWhenError("ORGANIZATION"));
+    }
 
     this.organizationSearchControllerApi = new OrganizationSearchControllerApi(apiClient);
     this.taxonomySearchControllerApi = new TaxonomySearchControllerApi(apiClient);
