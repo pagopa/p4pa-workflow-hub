0b19592f42dd09486c88262add4fb09f9e3c1fdd
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-02-20 16:17:25 +0100

    P4ADEV-2130 fix pr

--- src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/debtposition/wfingestion/DebtPositionIngestionFlowWFImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/debtposition/wfingestion/DebtPositionIngestionFlowWFImpl.java
@@ -1,13 +1,21 @@
 package it.gov.pagopa.pu.workflow.wf.ingestionflow.debtposition.wfingestion;
 
 import io.temporal.spring.boot.WorkflowImpl;
+import io.temporal.workflow.Workflow;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.IngestionFlowFileProcessingLockerActivity;
+import it.gov.pagopa.payhub.activities.activity.ingestionflow.UpdateIngestionFlowStatusActivity;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.debtposition.InstallmentIngestionFlowFileActivity;
+import it.gov.pagopa.payhub.activities.activity.ingestionflow.email.SendEmailIngestionFlowActivity;
+import it.gov.pagopa.payhub.activities.dto.debtposition.InstallmentIngestionFlowFileResult;
+import it.gov.pagopa.pu.processexecutions.dto.generated.IngestionFlowFile;
 import it.gov.pagopa.pu.workflow.wf.ingestionflow.debtposition.config.DebtPositionIngestionFlowWfConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+
+import java.time.Duration;
 
 @Slf4j
 @WorkflowImpl(taskQueues = DebtPositionIngestionFlowWFImpl.TASK_QUEUE_DEBT_POSITION_INGESTION_FLOW)
@@ -16,6 +24,8 @@ public class DebtPositionIngestionFlowWFImpl implements DebtPositionIngestionFlo
 
   private IngestionFlowFileProcessingLockerActivity ingestionFlowFileProcessingLockerActivity;
   private InstallmentIngestionFlowFileActivity installmentIngestionFlowFileActivity;
+  private UpdateIngestionFlowStatusActivity updateIngestionFlowStatusActivity;
+  private SendEmailIngestionFlowActivity sendEmailIngestionFlowActivity;
 
   /**
    * Temporal workflow will not allow to use injection in order to avoid <a href="https://docs.temporal.io/workflows#non-deterministic-change">non-deterministic changes</a> due to dynamic reconfiguration.<BR />
@@ -29,17 +39,38 @@ public class DebtPositionIngestionFlowWFImpl implements DebtPositionIngestionFlo
 
     ingestionFlowFileProcessingLockerActivity = wfConfig.buildIngestionFlowFileProcessingLockerActivityStub();
     installmentIngestionFlowFileActivity = wfConfig.buildInstallmentIngestionFlowFileActivityStub();
+    updateIngestionFlowStatusActivity = wfConfig.buildUpdateIngestionFlowStatusActivityStub();
+    sendEmailIngestionFlowActivity = wfConfig.buildSendEmailIngestionFlowActivityStub();
   }
 
   @Override
   public void ingest(Long ingestionFlowFileId) {
     log.info("Acquiring lock for ingestionFlowFileId {}", ingestionFlowFileId);
 
-    boolean result = ingestionFlowFileProcessingLockerActivity.acquireProcessingLock(ingestionFlowFileId);
-    log.info("Acquire Processing Lock result for ingestionFlowFileId {} is: {}", ingestionFlowFileId, result);
+    Workflow.await(Duration.ofSeconds(5),
+      () -> {
+        boolean result = ingestionFlowFileProcessingLockerActivity.acquireProcessingLock(ingestionFlowFileId);
+        if (!result) {
+          log.info("Lock not acquired, will retry for ingestionFlowFileId {}", ingestionFlowFileId);
+          return false;
+        }
+        return true;
+      }
+    );
 
-    if (result) {
-      installmentIngestionFlowFileActivity.processFile(ingestionFlowFileId);
-    }
+    log.info("Lock successfully acquired for ingestionFlowFileId {}", ingestionFlowFileId);
+    InstallmentIngestionFlowFileResult ingestionResult = installmentIngestionFlowFileActivity.processFile(ingestionFlowFileId);
+    boolean success = StringUtils.isEmpty(ingestionResult.getErrorDescription());
+
+    updateIngestionFlowStatusActivity.updateStatus(ingestionFlowFileId,
+      IngestionFlowFile.StatusEnum.PROCESSING,
+      success
+        ? IngestionFlowFile.StatusEnum.COMPLETED
+        : IngestionFlowFile.StatusEnum.ERROR,
+      ingestionResult.getErrorDescription(),
+      ingestionResult.getDiscardedFileName());
+    sendEmailIngestionFlowActivity.sendEmail(ingestionFlowFileId, success);
+
+    log.info("Debt Position ingestion with ID {} is completed", ingestionFlowFileId);
   }
 }
