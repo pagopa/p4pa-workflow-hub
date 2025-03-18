3f48c405bb7b443434bea2b2676a39fac8d07e74
antonio.torre <antonio.torre@pagopa.it>
2025-03-18 15:48:55 +0100

    P4ADEV-2434 workflow tables

---
+++ src/main/java/it/gov/pagopa/pu/workflow/model/WorkflowType.java
@@ -0,0 +1,33 @@
+package it.gov.pagopa.pu.workflow.model;
+
+import it.gov.pagopa.pu.workflow.model.executionconfig.WfExecutionConfig;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "workflow_type")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class WorkflowType extends BaseEntity implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "workflow_type_generator")
+  @SequenceGenerator(name = "workflow_type_generator", sequenceName = "workflow_type_seq", allocationSize = 1)
+  private Long workflowTypeId;
+  @NotNull
+  private String description;
+  @NotNull
+  private String workflowTypeName;
+  @JdbcTypeCode(SqlTypes.JSON)
+  private WfExecutionConfig defaultExecutionConfig;
+}
