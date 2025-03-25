64862d0e39664216c449d48d7090b29c4ff18d10
antonio.torre <antonio.torre@pagopa.it>
2025-03-25 17:34:20 +0100

    fix: PaymentEventRequestDTO Json deserialization

--- src/main/java/it/gov/pagopa/pu/workflow/dto/PaymentEventRequestDTO.java
+++ src/main/java/it/gov/pagopa/pu/workflow/dto/PaymentEventRequestDTO.java
@@ -3,12 +3,16 @@ package it.gov.pagopa.pu.workflow.dto;
 import it.gov.pagopa.pu.workflow.dto.generated.PaymentEventType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.io.Serializable;
+
+@NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
 @Data
-public class PaymentEventRequestDTO {
+public class PaymentEventRequestDTO implements Serializable {
   private PaymentEventType paymentEventType;
   private String eventDescription;
 }
