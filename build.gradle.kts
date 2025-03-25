7451d93a3cea469f2cda2aaf8fd3d3009eb02699
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-03-25 17:51:33 +0100

    fix version activity

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ val guavaVersion = "33.4.0-jre"
 val postgresJdbcVersion = "42.7.5"
 val podamVersion = "8.0.2.RELEASE"
 
-val activitiesVersion = "1.82.6-SNAPSHOT"
+val activitiesVersion = "1.82.6"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
