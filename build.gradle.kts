2f9d919b08d0f17e691137deb7949407bd829230
dgiacobbe <domenico.giacobbe@eng.it>
2025-03-28 09:11:05 +0100

    P4ADEV-2157 api for create assessments

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ val guavaVersion = "33.4.0-jre"
 val postgresJdbcVersion = "42.7.5"
 val podamVersion = "8.0.2.RELEASE"
 
-val activitiesVersion = "1.85.4"
+val activitiesVersion = "1.86.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
---
+++ src/main/java/it/gov/pagopa/pu/workflow/controller/AssessmentsControllerImpl.java
@@ -0,0 +1,28 @@
+package it.gov.pagopa.pu.workflow.controller;
+
+import it.gov.pagopa.pu.workflow.controller.generated.CreateAssessmentsApi;
+import it.gov.pagopa.pu.workflow.dto.generated.WorkflowCreatedDTO;
+import it.gov.pagopa.pu.workflow.wf.assessments.CreateAssessmentsWFClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class AssessmentsControllerImpl implements CreateAssessmentsApi {
+  private final CreateAssessmentsWFClient createAssessmentsWFClient;
+
+	public AssessmentsControllerImpl(CreateAssessmentsWFClient createAssessmentsWFClient) {
+		this.createAssessmentsWFClient = createAssessmentsWFClient;
+	}
+
+  @Override
+  public ResponseEntity<WorkflowCreatedDTO> createAssessmentsProcess(Long receiptId) {
+    log.info("Creating create assessments Workflow for receipt id {} ", receiptId);
+    String workflowId = createAssessmentsWFClient.createAssessments(receiptId);
+
+    WorkflowCreatedDTO response = new WorkflowCreatedDTO(workflowId);
+    log.info("workflow {} created successfully for receipt id {}", workflowId, receiptId);
+    return ResponseEntity.status(201).body(response);
+  }
+}
