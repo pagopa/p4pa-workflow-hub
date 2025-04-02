8822c23f95c5657e66e4efdbd8fc0301ac5fc382
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2025-04-02 12:07:38 +0200

    P4ADEV-2593-create-local-activity-for-dp-fine

--- src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/custom/fine/activity/InvokeSyncDebtPositionActivity.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/custom/fine/activity/InvokeSyncDebtPositionActivity.java
@@ -1,12 +1,12 @@
 package it.gov.pagopa.pu.workflow.wf.debtposition.custom.fine.activity;
 
+import io.temporal.activity.ActivityInterface;
 import io.temporal.activity.ActivityMethod;
-import io.temporal.workflow.WorkflowInterface;
 import it.gov.pagopa.payhub.activities.dto.debtposition.syncwfconfig.GenericWfExecutionConfig;
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
 import it.gov.pagopa.pu.workflow.dto.PaymentEventRequestDTO;
 
-@WorkflowInterface
+@ActivityInterface
 public interface InvokeSyncDebtPositionActivity {
 
   @ActivityMethod
