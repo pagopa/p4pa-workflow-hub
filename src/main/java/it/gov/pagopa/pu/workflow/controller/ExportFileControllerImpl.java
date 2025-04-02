3f71d77ce94ca0d7b52feae4174e00d20476eebb
pelliccm <matteo.pelliccione@nttdata.com>
2025-04-02 15:21:00 +0200

    [P4ADEV-2303] wf api export file invocation openApi

---
+++ src/main/java/it/gov/pagopa/pu/workflow/controller/ExportFileControllerImpl.java
@@ -0,0 +1,33 @@
+package it.gov.pagopa.pu.workflow.controller;
+
+import it.gov.pagopa.pu.processexecutions.dto.generated.ExportFile;
+import it.gov.pagopa.pu.workflow.controller.generated.ExportFileApi;
+import it.gov.pagopa.pu.workflow.dto.generated.WorkflowCreatedDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ExportFileControllerImpl implements ExportFileApi {
+
+  public ExportFileControllerImpl() {
+  }
+
+  @Override
+  public ResponseEntity<WorkflowCreatedDTO> exportFile(Long exportFileId, ExportFile.ExportFileTypeEnum flowFileType) {
+    log.info("Creating IngestionFlowFile Workflow for exportFileId {} of type {}", exportFileId, flowFileType);
+
+    // TODO: call workflow String workflowId = ;
+
+    /*WorkflowCreatedDTO response = new WorkflowCreatedDTO(workflowId);
+    response.setWorkflowId(workflowId);
+
+    log.info("Ingestion workflow {} created successfully for ingestionFileId {} of type {}", workflowId, exportFileId, flowFileType);
+
+    return ResponseEntity.status(201).body(response);*/
+
+    return ResponseEntity.ok(new WorkflowCreatedDTO());
+  }
+
+}
