6192a253ce2f13b71321882bfafd12ca51928457
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-04-02 12:39:36 +0200

    P4ADEV-2440 fix junit

--- src/test/java/it/gov/pagopa/pu/workflow/wf/pagopa/send/wfsendnotification/SendNotificationDateRetrieveWFImplTest.java
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/pagopa/send/wfsendnotification/SendNotificationDateRetrieveWFImplTest.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.pu.workflow.wf.pagopa.send.wfsendnotification;
 
+import io.temporal.workflow.Workflow;
 import it.gov.pagopa.payhub.activities.activity.sendnotification.*;
 import it.gov.pagopa.pu.sendnotification.dto.generated.SendNotificationDTO;
 import it.gov.pagopa.pu.workflow.wf.pagopa.send.config.SendNotificationProcessWfConfig;
@@ -8,11 +9,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationContext;
 
 import static it.gov.pagopa.pu.workflow.utils.faker.SendNotificationDTOFaker.buildSendNotificationDTO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class SendNotificationDateRetrieveWFImplTest {
@@ -58,5 +61,27 @@ class SendNotificationDateRetrieveWFImplTest {
     Mockito.verify(sendNotificationDateRetrieveActivityMock).sendNotificationDateRetrieve(sendNotificationId);
   }
 
-}
+  @Test
+  void givenSendNotificationIdWhenSendNotificationDateRetrieveThenRetriesUntilSuccess() {
+    // Given
+    String sendNotificationId = "testId";
+    SendNotificationDTO expectedResponse = buildSendNotificationDTO();
 
+    Mockito.when(sendNotificationDateRetrieveActivityMock.sendNotificationDateRetrieve(sendNotificationId))
+      .thenReturn(null)
+      .thenReturn(null)
+      .thenReturn(expectedResponse);
+
+    try (MockedStatic<Workflow> workflowMock = Mockito.mockStatic(Workflow.class)) {
+      workflowMock.when(() -> Workflow.sleep(Mockito.anyLong()))
+        .then(invocation -> null);
+
+      // When
+      SendNotificationDTO actualResponse = wf.sendNotificationDateRetrieve(sendNotificationId);
+
+      // Then
+      Mockito.verify(sendNotificationDateRetrieveActivityMock, Mockito.times(3)).sendNotificationDateRetrieve(sendNotificationId);
+      assertEquals(expectedResponse, actualResponse);
+    }
+  }
+}
