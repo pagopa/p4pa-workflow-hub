becbba25ee167af2061d7a6db6bae1da0666a2a4
ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
2025-04-02 18:24:54 +0200

    feat: P4ADEV-2315 create export file expiration handler wf (#96)

---
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/exportfile/expiration/config/ExportFileExpirationHandlerWFConfig.java
@@ -0,0 +1,17 @@
+package it.gov.pagopa.pu.workflow.wf.exportfile.expiration.config;
+
+import io.temporal.workflow.Workflow;
+import it.gov.pagopa.payhub.activities.activity.exportflow.ExportFileExpirationHandlerActivity;
+import it.gov.pagopa.pu.workflow.config.BaseWfConfig;
+import it.gov.pagopa.pu.workflow.config.TemporalWFImplementationCustomizer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "workflow.export-file-expiration-handler")
+public class ExportFileExpirationHandlerWFConfig extends BaseWfConfig {
+
+  public ExportFileExpirationHandlerActivity buildExportFileExpirationHandlerActivityStub() {
+    return Workflow.newActivityStub(ExportFileExpirationHandlerActivity.class, TemporalWFImplementationCustomizer.baseWfConfig2ActivityOptions(this));
+  }
+}
---
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/exportfile/expiration/ExportFileExpirationHandlerWFClientTest.java
@@ -0,0 +1,60 @@
+package it.gov.pagopa.pu.workflow.wf.exportfile.expiration;
+
+import it.gov.pagopa.pu.workflow.service.WorkflowService;
+import it.gov.pagopa.pu.workflow.utilities.Utilities;
+import it.gov.pagopa.pu.workflow.wf.exportfile.expiration.wfexpiration.ExportFileExpirationHandlerWFImpl;
+import it.gov.pagopa.pu.workflow.wf.exportfile.expiration.wfexpiration.ExportFileExpirationHandlerWF;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ExportFileExpirationHandlerWFClientTest {
+
+  @Mock
+  private WorkflowService workflowServiceMock;
+  @Mock
+  private ExportFileExpirationHandlerWF wfMock;
+
+  private ExportFileExpirationHandlerWFClient client;
+
+  @BeforeEach
+  void setUp() {
+    client = new ExportFileExpirationHandlerWFClient(workflowServiceMock);
+  }
+
+  @AfterEach
+  void verifyNoMoreInteractions() {
+    Mockito.verifyNoMoreInteractions(workflowServiceMock);
+  }
+
+  @Test
+  void testCreateExportFileExpirationHandler() {
+    Long exportFileId = 456L;
+    String taskQueue = ExportFileExpirationHandlerWFImpl.TASK_QUEUE_EXPORT_FILE_EXPIRATION_HANDLER_WF;
+    String expectedWorkflowId = "ExportFileExpirationHandlerWF-456";
+
+    try (MockedStatic<Utilities> utilitiesMockedStatic = mockStatic(Utilities.class)) {
+      utilitiesMockedStatic
+        .when(() -> Utilities.generateWorkflowId(exportFileId, taskQueue))
+        .thenReturn(expectedWorkflowId);
+
+      Mockito.when(workflowServiceMock.buildWorkflowStub(ExportFileExpirationHandlerWF.class, taskQueue, expectedWorkflowId))
+        .thenReturn(wfMock);
+
+      String workflowId = client.exportFileExpirationHandler(exportFileId);
+
+      assertEquals(expectedWorkflowId, workflowId);
+      verify(wfMock).exportFileExpirationHandler(exportFileId);
+    }
+  }
+}
