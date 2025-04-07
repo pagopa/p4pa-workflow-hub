7cb7d55646165696c68e214ab8580137c86bdcf3
pelliccm <107944358+pelliccm@users.noreply.github.com>
2025-04-07 11:12:34 +0200

    feat: P4ADEV-2303 wf api export file invocation openApi (#108)

--- src/test/java/it/gov/pagopa/pu/workflow/service/exportfile/ExportFileServiceImplTest.java
+++ src/test/java/it/gov/pagopa/pu/workflow/service/exportfile/ExportFileServiceImplTest.java
@@ -1,6 +1,9 @@
 package it.gov.pagopa.pu.workflow.service.exportfile;
 
+import it.gov.pagopa.pu.processexecutions.dto.generated.ExportFile;
+import it.gov.pagopa.pu.processexecutions.dto.generated.ExportFile.ExportFileTypeEnum;
 import it.gov.pagopa.pu.workflow.dto.generated.WorkflowCreatedDTO;
+import it.gov.pagopa.pu.workflow.wf.exportfile.ExportFileWFClient;
 import it.gov.pagopa.pu.workflow.wf.exportfile.expiration.ExportFileExpirationHandlerWFClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -15,13 +18,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class ExportFileServiceImplTest {
 
   @Mock
+  private ExportFileWFClient exportFileWFClientMock;
+  @Mock
   private ExportFileExpirationHandlerWFClient exportFileExpirationHandlerWFClientMock;
 
   private ExportFileService service;
 
   @BeforeEach
   void init(){
-    service = new ExportFileServiceImpl(exportFileExpirationHandlerWFClientMock);
+    service = new ExportFileServiceImpl(exportFileWFClientMock, exportFileExpirationHandlerWFClientMock);
   }
 
   @AfterEach
@@ -41,6 +46,23 @@ class ExportFileServiceImplTest {
       .thenReturn("WFID");
 
     WorkflowCreatedDTO result = service.expireExportFile(exportFileId);
+
+    Assertions.assertEquals(expectedResult, result);
+  }
+
+  @Test
+  void givenExportFileIdWhenCreateExportFileThenOk() {
+    Long exportFileId = 1L;
+
+    WorkflowCreatedDTO expectedResult = WorkflowCreatedDTO.builder()
+      .workflowId("WFID")
+      .build();
+
+    Mockito.when(exportFileWFClientMock.exportFile(Mockito.same(exportFileId), Mockito.any(
+        ExportFile.ExportFileTypeEnum.class)))
+      .thenReturn("WFID");
+
+    WorkflowCreatedDTO result = service.exportFile(exportFileId, ExportFileTypeEnum.PAID);
 
     Assertions.assertEquals(expectedResult, result);
   }
