35cb53efde8137c22126541bbdff175728c52985
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-04-08 16:57:20 +0200

    P4PADEV-2326 updated version of activities

--- src/main/java/it/gov/pagopa/pu/workflow/config/RestTemplateConfigImport.java
+++ src/main/java/it/gov/pagopa/pu/workflow/config/RestTemplateConfigImport.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(it.gov.pagopa.payhub.activities.config.RestTemplateConfig.class)
+@Import(it.gov.pagopa.payhub.activities.config.rest.RestTemplateConfig.class)
 public class RestTemplateConfigImport {
 }
