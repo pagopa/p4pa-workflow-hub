e49a6b533e37d6494891c5a2ce995e3e8c30446f
domenicogi <domenico.giacobbe@eng.it>
2025-04-10 14:00:46 +0200

    Update src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/paymentnotification/wfingestion/PaymentNotificationIngestionWF.java
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/paymentnotification/wfingestion/PaymentNotificationIngestionWF.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/paymentnotification/wfingestion/PaymentNotificationIngestionWF.java
@@ -4,7 +4,7 @@ import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 
 /**
- * Workflow to ingest PaymentsReporting file
+ * Workflow to ingest PaymentNotification file
  * @see <a href=https://pagopa.atlassian.net/wiki/spaces/SPAC/pages/1339031693/Classificazione+incassi#3.2.-Pagamento-Notificati>Confluence page</a>
  */
 @WorkflowInterface
