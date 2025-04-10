242d05afa76dfdf550fa2c493b4da9fda1c2b2b0
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2025-04-10 10:51:35 -0300

    P4ADEV-2644-fine-wf

--- src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/custom/activity/ScheduleReductionExpirationActivity.java
+++
@@ -1,8 +0,0 @@
-package it.gov.pagopa.pu.workflow.wf.debtposition.custom.activity;
-
-import it.gov.pagopa.payhub.activities.dto.debtposition.syncwfconfig.FineWfExecutionConfig;
-
-public interface ScheduleReductionExpirationActivity {
-
-  String expireFineReduction(Long debtPositionId, FineWfExecutionConfig executionParams);
-}
