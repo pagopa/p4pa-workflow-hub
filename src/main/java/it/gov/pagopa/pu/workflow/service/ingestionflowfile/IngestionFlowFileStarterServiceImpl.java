3b3cec499348545c9c10d8f9d4edb58b67c36ab4
dgiacobbe <domenico.giacobbe@eng.it>
2025-04-13 23:55:35 +0200

    P4ADEV-2014 code review

--- src/main/java/it/gov/pagopa/pu/workflow/service/ingestionflowfile/IngestionFlowFileStarterServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/service/ingestionflowfile/IngestionFlowFileStarterServiceImpl.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.pu.workflow.service.ingestionflowfile;
 import it.gov.pagopa.payhub.activities.exception.ingestionflow.IngestionFlowTypeNotSupportedException;
 import it.gov.pagopa.pu.processexecutions.dto.generated.IngestionFlowFile;
 import it.gov.pagopa.pu.workflow.wf.ingestionflow.debtposition.DebtPositionIngestionWFClient;
+import it.gov.pagopa.pu.workflow.wf.ingestionflow.paymentnotification.PaymentNotificationIngestionWFClient;
 import it.gov.pagopa.pu.workflow.wf.ingestionflow.paymentsreporting.PaymentsReportingIngestionWFClient;
 import it.gov.pagopa.pu.workflow.wf.ingestionflow.receipt.pagopa.ReceiptPagopaIngestionWFClient;
 import it.gov.pagopa.pu.workflow.wf.ingestionflow.treasury.opi.TreasuryOpiIngestionWFClient;
@@ -20,12 +21,15 @@ public class IngestionFlowFileStarterServiceImpl implements IngestionFlowFileSta
     PaymentsReportingIngestionWFClient paymentsReportingIngestionWFClient,
     TreasuryOpiIngestionWFClient treasuryOpiIngestionWFClient,
     DebtPositionIngestionWFClient debtPositionIngestionWFClient,
-    ReceiptPagopaIngestionWFClient receiptPagopaIngestionWFClient) {
+    ReceiptPagopaIngestionWFClient receiptPagopaIngestionWFClient,
+    PaymentNotificationIngestionWFClient paymentNotificationIngestionWFClient) {
     ingestionFlowFileType2WfStarter = Map.of(
       IngestionFlowFile.IngestionFlowFileTypeEnum.PAYMENTS_REPORTING, paymentsReportingIngestionWFClient::ingest,
       IngestionFlowFile.IngestionFlowFileTypeEnum.TREASURY_OPI, treasuryOpiIngestionWFClient::ingest,
       IngestionFlowFile.IngestionFlowFileTypeEnum.DP_INSTALLMENTS, debtPositionIngestionWFClient::ingest,
-      IngestionFlowFile.IngestionFlowFileTypeEnum.RECEIPT_PAGOPA, receiptPagopaIngestionWFClient::ingest
+      IngestionFlowFile.IngestionFlowFileTypeEnum.RECEIPT_PAGOPA, receiptPagopaIngestionWFClient::ingest,
+      IngestionFlowFile.IngestionFlowFileTypeEnum.PAYMENT_NOTIFICATION, paymentNotificationIngestionWFClient::ingest
+
     );
   }
 
