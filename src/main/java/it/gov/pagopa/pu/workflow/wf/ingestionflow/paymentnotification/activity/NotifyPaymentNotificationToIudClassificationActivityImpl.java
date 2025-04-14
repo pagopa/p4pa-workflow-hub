37f6908a9a2c6afaa0df1278f3898c011a191e99
macacia <marco.cacia@eng.it>
2025-04-14 16:15:52 +0200

    P4ADEV-1570 signal service

--- src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/paymentnotification/activity/NotifyPaymentNotificationToIudClassificationActivityImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/paymentnotification/activity/NotifyPaymentNotificationToIudClassificationActivityImpl.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.pu.workflow.wf.ingestionflow.paymentnotification.activity;
 
 import io.temporal.spring.boot.ActivityImpl;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.IudClassificationWFClient;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.dto.IudClassificationNotifyPaymentNotificationSignalDTO;
 import it.gov.pagopa.pu.workflow.wf.ingestionflow.paymentnotification.wfingestion.PaymentNotificationIngestionWFImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,20 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @ActivityImpl(taskQueues = PaymentNotificationIngestionWFImpl.TASK_QUEUE_PAYMENT_NOTIFICATION_INGESTION_LOCAL_ACTIVITY)
 public class NotifyPaymentNotificationToIudClassificationActivityImpl implements NotifyPaymentNotificationToIudClassificationActivity {
+  private final IudClassificationWFClient iudClassificationWFClient;
 
+  public NotifyPaymentNotificationToIudClassificationActivityImpl(IudClassificationWFClient iudClassificationWFClient) {
+    this.iudClassificationWFClient = iudClassificationWFClient;
+  }
 
   @Override
   public void signalIudClassificationWithStart(Long organizationId, String iud) {
-    //todo to be implemented after the iud classification workflow completion P4ADEV-1571
+    IudClassificationNotifyPaymentNotificationSignalDTO signalDTO =
+      IudClassificationNotifyPaymentNotificationSignalDTO.builder()
+        .organizationId(organizationId)
+        .iud(iud)
+        .build();
+    iudClassificationWFClient.notifyPaymentNotification(signalDTO);
   }
 }
 
---
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/paymentnotification/activity/NotifyPaymentNotificationToIudClassificationActivityImplTest.java
@@ -0,0 +1,42 @@
+package it.gov.pagopa.pu.workflow.wf.ingestionflow.paymentnotification.activity;
+
+import it.gov.pagopa.pu.workflow.wf.classification.iud.IudClassificationWFClient;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.dto.IudClassificationNotifyPaymentNotificationSignalDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class NotifyPaymentNotificationToIudClassificationActivityImplTest {
+
+  @Mock
+  private IudClassificationWFClient iudClassificationWFClientMock;
+
+  private NotifyPaymentNotificationToIudClassificationActivity activity;
+
+  @BeforeEach
+  void setUp() {
+    activity = new NotifyPaymentNotificationToIudClassificationActivityImpl(iudClassificationWFClientMock);
+  }
+
+  @Test
+  void testSignalIudClassificationWithStart() {
+    // Given
+    Long organizationId = 1L;
+    String iud = "iud-123";
+    IudClassificationNotifyPaymentNotificationSignalDTO expectedSignalDTO = IudClassificationNotifyPaymentNotificationSignalDTO.builder()
+      .organizationId(organizationId)
+      .iud(iud)
+      .build();
+
+    // When
+    activity.signalIudClassificationWithStart(organizationId, iud);
+
+    // Then
+    verify(iudClassificationWFClientMock).notifyPaymentNotification(expectedSignalDTO);
+  }
+}
