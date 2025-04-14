9534b2626df890af3c43f5845eeaef247f17b408
macacia <marco.cacia@eng.it>
2025-04-14 16:16:14 +0200

    P4ADEV-1570 client

---
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/classification/iud/IudClassificationWFClient.java
@@ -0,0 +1,58 @@
+package it.gov.pagopa.pu.workflow.wf.classification.iud;
+
+import io.temporal.api.common.v1.WorkflowExecution;
+import io.temporal.client.WorkflowStub;
+import it.gov.pagopa.pu.workflow.exception.custom.WorkflowInternalErrorException;
+import it.gov.pagopa.pu.workflow.service.WorkflowService;
+import it.gov.pagopa.pu.workflow.utilities.Utilities;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.dto.IudClassificationNotifyPaymentNotificationSignalDTO;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.dto.IudClassificationNotifyReceiptSignalDTO;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.wfclassification.IudClassificationWF;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.wfclassification.IudClassificationWFImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class IudClassificationWFClient {
+  private final WorkflowService workflowService;
+
+  public IudClassificationWFClient(WorkflowService workflowService) {
+    this.workflowService = workflowService;
+  }
+
+  public String notifyReceipt(IudClassificationNotifyReceiptSignalDTO signalDTO) {
+    String workflowId = generateWorkflowId(signalDTO.getOrgId(), signalDTO.getIud());
+
+    WorkflowStub untypedWorkflowStub = workflowService.buildUntypedWorkflowStub(IudClassificationWFImpl.TASK_QUEUE_IUF_CLASSIFICATION_WF, workflowId);
+    WorkflowExecution wfExecution = untypedWorkflowStub.signalWithStart(
+      IudClassificationWF.SIGNAL_METHOD_NAME_NOTIFY_RECEIPT,
+      new Object[]{signalDTO},
+      new Object[]{}
+    );
+
+    log.info("IUD classification Workflow for Receipt started with workflowId: {}", wfExecution.getWorkflowId());
+    return workflowId;
+  }
+
+  public String notifyPaymentNotification(IudClassificationNotifyPaymentNotificationSignalDTO signalDTO) {
+    String workflowId = generateWorkflowId(signalDTO.getOrganizationId(), signalDTO.getIud());
+
+    WorkflowStub untypedWorkflowStub = workflowService.buildUntypedWorkflowStub(IudClassificationWFImpl.TASK_QUEUE_IUF_CLASSIFICATION_WF, workflowId);
+    WorkflowExecution wfExecution = untypedWorkflowStub.signalWithStart(
+      IudClassificationWF.SIGNAL_METHOD_NAME_NOTIFY_PAYMENT_NOTIFICATION,
+      new Object[]{signalDTO},
+      new Object[]{}
+    );
+
+    log.info("IUD classification Workflow for Notification started with workflowId: {}", wfExecution.getWorkflowId());
+    return workflowId;
+  }
+
+  private static String generateWorkflowId(Long organizationId, String iud) {
+    if (organizationId == null || iud == null) {
+      throw new WorkflowInternalErrorException("The organizationId or iud must not be null");
+    }
+    return Utilities.generateWorkflowId(String.format("%d-%s", organizationId, iud), IudClassificationWF.class);
+  }
+}
---
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/classification/iud/IudClassificationWFClientTest.java
@@ -0,0 +1,115 @@
+package it.gov.pagopa.pu.workflow.wf.classification.iud;
+
+import io.temporal.api.common.v1.WorkflowExecution;
+import io.temporal.client.WorkflowStub;
+import it.gov.pagopa.pu.workflow.service.WorkflowService;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.dto.IudClassificationNotifyPaymentNotificationSignalDTO;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.dto.IudClassificationNotifyReceiptSignalDTO;
+import it.gov.pagopa.pu.workflow.wf.classification.iud.wfclassification.IudClassificationWF;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+class IudClassificationWFClientTest {
+  @Mock
+  private WorkflowService workflowServiceMock;
+  @Mock
+  private WorkflowStub workflowStubMock;
+  @Mock
+  private WorkflowExecution workflowExecutionMock;
+
+  private IudClassificationWFClient client;
+
+  @BeforeEach
+  void setUp() {
+    client = new IudClassificationWFClient(workflowServiceMock);
+  }
+
+  @AfterEach
+  void tearDown() {
+    verifyNoMoreInteractions(workflowServiceMock);
+  }
+
+  @Test
+  void notifyReceipt() {
+    // Given
+    IudClassificationNotifyReceiptSignalDTO signalDTO = IudClassificationNotifyReceiptSignalDTO.builder()
+      .orgId(1L)
+      .iud("iud123")
+      .iur("iur123")
+      .iuv("iuv123")
+      .transferIndex(1)
+      .build();
+
+    String expectedWorkflowId = "IudClassificationWF-1-iud123";
+
+    Mockito.when(workflowServiceMock.buildUntypedWorkflowStub(any(String.class), any(String.class)))
+      .thenReturn(workflowStubMock);
+    Mockito.when(workflowStubMock.signalWithStart(
+        eq(IudClassificationWF.SIGNAL_METHOD_NAME_NOTIFY_RECEIPT),
+        any(),
+        any()))
+      .thenReturn(workflowExecutionMock);
+
+    // When
+    String workflowId = client.notifyReceipt(signalDTO);
+
+    // Then
+    assertEquals(expectedWorkflowId, workflowId);
+  }
+
+  @Test
+  void notifyPaymentNotification() {
+    // Given
+    IudClassificationNotifyPaymentNotificationSignalDTO signalDTO = IudClassificationNotifyPaymentNotificationSignalDTO.builder()
+      .organizationId(1L)
+      .iud("iud123")
+      .build();
+
+    String expectedWorkflowId = "IudClassificationWF-1-iud123";
+
+    Mockito.when(workflowServiceMock.buildUntypedWorkflowStub(any(String.class), any(String.class)))
+      .thenReturn(workflowStubMock);
+    Mockito.when(workflowStubMock.signalWithStart(
+        eq(IudClassificationWF.SIGNAL_METHOD_NAME_NOTIFY_PAYMENT_NOTIFICATION),
+        any(),
+        any()))
+      .thenReturn(workflowExecutionMock);
+
+    // When
+    String workflowId = client.notifyPaymentNotification(signalDTO);
+
+    // Then
+    assertEquals(expectedWorkflowId, workflowId);
+  }
+
+  @Test
+  void testSignalMethodsExist() {
+    assertDoesNotThrow(() -> {
+      checkMethodExistsOnWfAndClientClasses(
+        IudClassificationWF.SIGNAL_METHOD_NAME_NOTIFY_RECEIPT,
+        IudClassificationNotifyReceiptSignalDTO.class);
+
+      checkMethodExistsOnWfAndClientClasses(
+        IudClassificationWF.SIGNAL_METHOD_NAME_NOTIFY_PAYMENT_NOTIFICATION,
+        IudClassificationNotifyPaymentNotificationSignalDTO.class);
+    });
+  }
+
+  // Helper method to check that the method exists on both classes
+  private void checkMethodExistsOnWfAndClientClasses(String methodName, Class<?>... parameterTypes) throws NoSuchMethodException {
+    Assertions.assertNotNull(IudClassificationWF.class.getMethod(methodName, parameterTypes));
+    Assertions.assertNotNull(IudClassificationWFClient.class.getMethod(methodName, parameterTypes));
+  }
+}
