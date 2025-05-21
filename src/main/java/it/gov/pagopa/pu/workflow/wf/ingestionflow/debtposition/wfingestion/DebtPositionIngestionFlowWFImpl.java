a30b69960be4b809c271f418b803664cb2371de7
RiccardoGiuliani <144138935+RiccardoGiuliani@users.noreply.github.com>
2025-05-21 16:23:14 +0200

    feat: P4ADEV-2702 modified wf debt position ingestion flow (#143)

--- src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/debtposition/wfingestion/DebtPositionIngestionFlowWFImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/debtposition/wfingestion/DebtPositionIngestionFlowWFImpl.java
@@ -5,6 +5,7 @@ import io.temporal.workflow.Workflow;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.IngestionFlowFileProcessingLockerActivity;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.UpdateIngestionFlowStatusActivity;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.debtposition.InstallmentIngestionFlowFileActivity;
+import it.gov.pagopa.payhub.activities.activity.ingestionflow.debtposition.MassiveNoticeGenerationStatusRetrieverActivity;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.debtposition.SynchronizeIngestedDebtPositionActivity;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.email.SendEmailIngestionFlowActivity;
 import it.gov.pagopa.payhub.activities.dto.ingestion.IngestionFlowFileResult;
@@ -34,12 +35,15 @@ public class DebtPositionIngestionFlowWFImpl implements DebtPositionIngestionFlo
    * The threshold is very high ({@link Constants#THRESHOLD_TEMPORAL_EVENTS_BEFORE_CONTINUE_AS_NEW}), lock acquire is the first activity called, we are not interested on WF history, we will clear it before real limit
    */
   private static final int LOCK_ATTEMPTS_BEFORE_CLEAN_WF_HISTORY = 1000;
+  private static final int MAX_ATTEMPTS = 30;
+  private static final Duration RETRY_INTERVAL = Duration.ofSeconds(30);
 
   private IngestionFlowFileProcessingLockerActivity ingestionFlowFileProcessingLockerActivity;
   private InstallmentIngestionFlowFileActivity installmentIngestionFlowFileActivity;
   private UpdateIngestionFlowStatusActivity updateIngestionFlowStatusActivity;
   private SendEmailIngestionFlowActivity sendEmailIngestionFlowActivity;
   private SynchronizeIngestedDebtPositionActivity synchronizeIngestedDebtPositionActivity;
+  private MassiveNoticeGenerationStatusRetrieverActivity massiveNoticeGenerationStatusRetrieverActivity;
 
   /**
    * Temporal workflow will not allow to use injection in order to avoid <a href="https://docs.temporal.io/workflows#non-deterministic-change">non-deterministic changes</a> due to dynamic reconfiguration.<BR />
@@ -56,6 +60,7 @@ public class DebtPositionIngestionFlowWFImpl implements DebtPositionIngestionFlo
     updateIngestionFlowStatusActivity = wfConfig.buildUpdateIngestionFlowStatusActivityStub();
     sendEmailIngestionFlowActivity = wfConfig.buildSendEmailIngestionFlowActivityStub();
     synchronizeIngestedDebtPositionActivity = wfConfig.buildSynchronizeIngestedDebtPositionActivityStub();
+    massiveNoticeGenerationStatusRetrieverActivity = wfConfig.buildMassiveNoticeGenerationStatusRetrieverActivity();
   }
 
   @Override
@@ -71,6 +76,10 @@ public class DebtPositionIngestionFlowWFImpl implements DebtPositionIngestionFlo
 
     mergeErrorDescriptions(ingestionResult, syncDpResult.getErrorsDescription());
     boolean success = ingestionResult.getErrorDescription() == null;
+
+    if (StringUtils.isNotBlank(syncDpResult.getPdfGeneratedId()) && success) {
+      retrieveNoticesGenerationStatus(ingestionResult, syncDpResult.getPdfGeneratedId());
+    }
 
     updateIngestionFlowStatusActivity.updateStatus(ingestionFlowFileId,
       IngestionFlowFileStatus.PROCESSING,
@@ -122,4 +131,22 @@ public class DebtPositionIngestionFlowWFImpl implements DebtPositionIngestionFlo
     }
   }
 
+  private void retrieveNoticesGenerationStatus(InstallmentIngestionFlowFileResult ingestionResult, String pdfGeneratedId) {
+    int attemptCounter = 0;
+    while (attemptCounter < MAX_ATTEMPTS &&
+      massiveNoticeGenerationStatusRetrieverActivity.retrieveNoticesGenerationStatus(ingestionResult.getOrganizationId(), pdfGeneratedId) == null) {
+      attemptCounter++;
+
+      log.info("Generation status not retrieved, retrying for pdfGeneratedId {} (attempt {})", pdfGeneratedId, attemptCounter);
+      Workflow.sleep(RETRY_INTERVAL);
+    }
+
+    if (attemptCounter >= MAX_ATTEMPTS) {
+      String errorMessage = String.format("Max attempts reached for pdfGeneratedId %s. Unable to retrieve generation status.", pdfGeneratedId);
+      log.error(errorMessage);
+      mergeErrorDescriptions(ingestionResult, errorMessage);
+    } else {
+      log.info("Generation status retrieved for pdfGeneratedId {}", pdfGeneratedId);
+    }
+  }
 }
