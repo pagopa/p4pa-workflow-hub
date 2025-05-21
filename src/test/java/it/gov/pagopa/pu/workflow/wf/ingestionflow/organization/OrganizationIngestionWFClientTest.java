be5fec80681532ba9bb7c7181537f167e309d669
dgiacobbe <domenico.giacobbe@eng.it>
2025-05-21 16:16:58 +0200

    P4ADEV-2868 organization ingestion wf

---
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/organization/OrganizationIngestionWFClientTest.java
@@ -0,0 +1,59 @@
+package it.gov.pagopa.pu.workflow.wf.ingestionflow.organization;
+
+import it.gov.pagopa.pu.workflow.dto.generated.WorkflowCreatedDTO;
+import it.gov.pagopa.pu.workflow.service.temporal.WorkflowClientService;
+import it.gov.pagopa.pu.workflow.service.temporal.WorkflowService;
+import it.gov.pagopa.pu.workflow.utils.TemporalTestUtils;
+import it.gov.pagopa.pu.workflow.wf.ingestionflow.organization.wfingestion.OrganizationIngestionWF;
+import it.gov.pagopa.pu.workflow.wf.ingestionflow.organization.wfingestion.OrganizationIngestionWFImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class OrganizationIngestionWFClientTest {
+
+  @Mock
+  private WorkflowService workflowServiceMock;
+  @Mock
+  private WorkflowClientService workflowClientServiceMock;
+  @Mock
+  private OrganizationIngestionWF wfMock;
+
+  private OrganizationIngestionWFClient client;
+
+  @BeforeEach
+  void init() {
+    client = new OrganizationIngestionWFClient(workflowServiceMock, workflowClientServiceMock);
+  }
+
+  @AfterEach
+  void verifyNoMoreInteractions() {
+    Mockito.verifyNoMoreInteractions(workflowServiceMock, workflowClientServiceMock);
+  }
+
+  @Test
+  void whenIngestThenOk() {
+    // Given
+    long ingestionFlowFileId = 1L;
+    String taskQueue = OrganizationIngestionWFImpl.TASK_QUEUE_ORGANIZATION_INGESTION_WF;
+    WorkflowCreatedDTO expectedResult = new WorkflowCreatedDTO("OrganizationIngestionWF-1", "RUNID");
+
+    Mockito.when(workflowServiceMock.buildWorkflowStub(OrganizationIngestionWF.class, taskQueue, expectedResult.getWorkflowId()))
+      .thenReturn(wfMock);
+
+    TemporalTestUtils.configureWorkflowClientServiceMock(workflowClientServiceMock, expectedResult, ingestionFlowFileId);
+
+    // When
+    WorkflowCreatedDTO result = client.ingest(ingestionFlowFileId);
+
+    // Then
+    Assertions.assertEquals(expectedResult, result);
+    Mockito.verify(wfMock).ingest(ingestionFlowFileId);
+  }
+}
