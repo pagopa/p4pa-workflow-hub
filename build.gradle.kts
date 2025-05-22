2fef71d6ac98f579592ad3ec1e9395014e0494da
dgiacobbe <domenico.giacobbe@eng.it>
2025-05-22 10:12:42 +0200

    P4ADEV-2872 update activity version

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ val guavaVersion = "33.4.8-jre"
 val postgresJdbcVersion = "42.7.5"
 val podamVersion = "8.0.2.RELEASE"
 
-val p4paActivitiesVersion = "1.113.0"
+val p4paActivitiesVersion = "1.113.0-SNAPSHOT"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
