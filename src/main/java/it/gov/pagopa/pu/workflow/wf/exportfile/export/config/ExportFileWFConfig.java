d10a4c1b67b6e11a9bc1b731b5c7a48a1ef819da
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-06-05 15:30:11 +0200

    P4PADEV-2597 updated activities version and added SendEmailExportFileActivity

--- src/main/java/it/gov/pagopa/pu/workflow/wf/exportfile/export/config/ExportFileWFConfig.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/exportfile/export/config/ExportFileWFConfig.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.pu.workflow.wf.exportfile.export.config;
 import io.temporal.workflow.Workflow;
 import it.gov.pagopa.payhub.activities.activity.exportflow.ExportFileActivity;
 import it.gov.pagopa.payhub.activities.activity.exportflow.UpdateExportFileStatusActivity;
+import it.gov.pagopa.payhub.activities.activity.exportflow.email.SendEmailExportFileActivity;
 import it.gov.pagopa.pu.workflow.config.temporal.BaseWfConfig;
 import it.gov.pagopa.pu.workflow.config.temporal.TemporalWFImplementationCustomizer;
 import it.gov.pagopa.pu.workflow.wf.exportfile.export.activity.ScheduleExportFileExpirationActivity;
@@ -20,6 +21,10 @@ public class ExportFileWFConfig extends BaseWfConfig {
 
   public UpdateExportFileStatusActivity buildUpdateExportFileStatusActivityStub() {
     return Workflow.newActivityStub(UpdateExportFileStatusActivity.class, TemporalWFImplementationCustomizer.baseWfConfig2ActivityOptions(this));
+  }
+
+  public SendEmailExportFileActivity buildSendEmailExportFileActivityStub() {
+    return Workflow.newActivityStub(SendEmailExportFileActivity.class, TemporalWFImplementationCustomizer.baseWfConfig2ActivityOptions(this));
   }
 
   public ScheduleExportFileExpirationActivity buildScheduleExportFileExpirationActivityStub() {
