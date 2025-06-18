b942ccddf9c6ab12349dbd9d15b022b1fc3c7516
Antonio Calò <antonio.calo@nttdata.com>
2025-06-18 17:40:57 +0200

    Update src/main/java/it/gov/pagopa/pu/workflow/wf/assessments/wfassessments/CreateAssessmentsRegistryWF.java
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/pu/workflow/wf/assessments/wfassessments/CreateAssessmentsRegistryWF.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/assessments/wfassessments/CreateAssessmentsRegistryWF.java
@@ -5,6 +5,10 @@ import io.temporal.workflow.WorkflowMethod;
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
 import java.util.List;
 
+/**
+ * Workflow interface for the Assessment creation
+ * @see <a href=https://pagopa.atlassian.net/wiki/spaces/SPAC/pages/1816559617/Creazione+Registro+Accertamenti>Confluence page</a>
+ * */
 @WorkflowInterface
 public interface CreateAssessmentsRegistryWF {
 
