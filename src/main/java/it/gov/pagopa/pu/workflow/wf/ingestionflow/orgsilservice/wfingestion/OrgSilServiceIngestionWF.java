6d0bb024a1d5ec29ebdecaef00722beb7cb259b4
dgiacobbe <domenico.giacobbe@eng.it>
2025-07-14 17:08:39 +0200

    P4ADEV-2985 orgsilservice ingestion

---
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/orgsilservice/wfingestion/OrgSilServiceIngestionWF.java
@@ -0,0 +1,18 @@
+package it.gov.pagopa.pu.workflow.wf.ingestionflow.orgsilservice.wfingestion;
+
+import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
+import it.gov.pagopa.pu.workflow.wf.ingestionflow.BaseIngestionFlowFileWF;
+
+/**
+ * Workflow to ingest Organization file
+ * * @see <a href=https://pagopa.atlassian.net/wiki/spaces/SPAC/pages/1584595096/Specifica+di+integrazione+lato+EC+Intermediario#Tracciato---definizione-tracciato-ente-sil-service>Confluence page</a>
+ */
+
+
+@WorkflowInterface
+public interface OrgSilServiceIngestionWF extends BaseIngestionFlowFileWF {
+  @Override
+  @WorkflowMethod
+  void ingest(Long ingestionFlowFileId);
+}
