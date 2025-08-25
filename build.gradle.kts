e0a0671f67c68513cfdde28ada39fc0aaa04b5e3
Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>
2025-08-25 14:35:46 +0200

    fix: P4ADEV-3687 fix CVE-2025-48989 (#222)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.util.*
 
 plugins {
   java
-  id("org.springframework.boot") version "3.5.3"
+  id("org.springframework.boot") version "3.5.5"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "6.2.0.5505"
@@ -60,7 +60,7 @@ val postgresJdbcVersion = "42.7.7"
 val podamVersion = "8.0.2.RELEASE"
 val caffeineVersion = "3.2.1"
 
-val p4paActivitiesVersion = "1.141.0"
+val p4paActivitiesVersion = "1.141.2"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
