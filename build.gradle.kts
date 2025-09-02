72186871bff93984f662485778797fe26f34512c
Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>
2025-09-02 11:08:47 +0200

    fix: P4ADEV-3742 fix version activities (#237)

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ val postgresJdbcVersion = "42.7.7"
 val podamVersion = "8.0.2.RELEASE"
 val caffeineVersion = "3.2.1"
 
-val p4paActivitiesVersion = "1.143.1"
+val p4paActivitiesVersion = "1.143.2"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
