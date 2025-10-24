ab11fe09fa118d8984d74388ffcda23dbe4c988a
antoniocalo <antonio.calo@nttdata.com>
2025-10-24 13:03:28 +0200

    P4ADEV-4027-handle-classify-assessments

--- src/test/java/it/gov/pagopa/pu/workflow/wf/classification/transfer/config/TransferClassificationWfConfigTest.java
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/classification/transfer/config/TransferClassificationWfConfigTest.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.pu.workflow.wf.classification.transfer.config;
 
 import it.gov.pagopa.pu.workflow.utils.TemporalTestUtils;
+import it.gov.pagopa.pu.workflow.wf.classification.assessments.activity.StartAssessmentClassificationActivity;
+import it.gov.pagopa.pu.workflow.wf.classification.assessments.activity.StartAssessmentClassificationActivityImpl;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
@@ -10,7 +12,8 @@ class TransferClassificationWfConfigTest {
 
   private final TransferClassificationWfConfig config = new TransferClassificationWfConfig();
 
-  private final Map<Class<?>, Class<?>> localActivityInterface2Impl = Map.of();
+  private final Map<Class<?>, Class<?>> localActivityInterface2Impl = Map.of(
+    StartAssessmentClassificationActivity.class, StartAssessmentClassificationActivityImpl.class);
 
   @Test
   void testTaskQueueAlignment() throws InvocationTargetException, IllegalAccessException {
