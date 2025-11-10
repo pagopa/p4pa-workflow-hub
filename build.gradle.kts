b29704f17789fdc14518062495dc85330d3cf391
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2025-11-10 12:56:23 +0100

    fix: P4ADEV-4148 update activities version (#315)

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ val podamVersion = "8.0.2.RELEASE"
 val caffeineVersion = "3.2.2"
 val commonsLang3Version = "3.19.0"
 
-val p4paActivitiesVersion = "1.159.15"
+val p4paActivitiesVersion = "1.159.16"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
