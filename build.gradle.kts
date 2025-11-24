697805ffe62bc657974df46df62b4b709e9c4d19
michele.mazzini <michele.mazzini1996@gmail.com>
2025-11-24 09:29:50 +0100

    fix: use temporary activities version

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ val podamVersion = "8.0.2.RELEASE"
 val caffeineVersion = "3.2.2"
 val commonsLang3Version = "3.19.0"
 
-val p4paActivitiesVersion = "P4ADEV3983-SNAPSHOT"
+val p4paActivitiesVersion = "P4ADEV3983V2-SNAPSHOT"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
