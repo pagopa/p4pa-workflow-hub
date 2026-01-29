0d73c0ad803f00b4e55e620a9283c9a5e1f781a8
ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
2026-01-29 16:26:03 +0100

    fix: P4ADEV-4386 update activities version (#362)

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,7 +76,7 @@ val commonsLang3Version = "3.20.0"
 val lz4JavaVersion = "1.10.1"
 val springCloudDepsVersion = "2025.1.0"
 
-val p4paActivitiesVersion = "1.170.0"
+val p4paActivitiesVersion = "1.171.1"
 
 dependencyManagement {
   imports {
