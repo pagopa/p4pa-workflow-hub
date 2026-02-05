412fdac05f43ed2ab22a830bca899dbc14b3bd8b
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-02-05 09:51:30 +0100

    P4ADEV-3720: fix SendNotificationStreamConsumeWFImplTest

--- src/test/java/it/gov/pagopa/pu/workflow/wf/pagopa/send/wfsendnotification/SendNotificationStreamConsumeWFImplTest.java
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/pagopa/send/wfsendnotification/SendNotificationStreamConsumeWFImplTest.java
@@ -1,34 +1,29 @@
 package it.gov.pagopa.pu.workflow.wf.pagopa.send.wfsendnotification;
 
-import it.gov.pagopa.pu.workflow.wf.pagopa.send.config.SendNotificationProcessWfConfig;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationContext;
 
 @ExtendWith(MockitoExtension.class)
 class SendNotificationStreamConsumeWFImplTest {
 
+  @Mock
+  private ApplicationContext applicationContextMock;
+
   private SendNotificationStreamConsumeWFImpl wf;
 
   @BeforeEach
   void setUp() {
-    SendNotificationProcessWfConfig wfConfigMock = Mockito.mock(SendNotificationProcessWfConfig.class);
-    ApplicationContext applicationContextMock = Mockito.mock(ApplicationContext.class);
-
-    Mockito.when(applicationContextMock.getBean(SendNotificationProcessWfConfig.class)).thenReturn(wfConfigMock);
-
     wf = new SendNotificationStreamConsumeWFImpl();
     wf.setApplicationContext(applicationContextMock);
   }
 
   @AfterEach
-  void verifyNoMoreInteractions() {
-    Mockito.verifyNoMoreInteractions();
-  }
+  void verifyNoMoreInteractions() {}
 
   @Test
   void readSendStream() {
