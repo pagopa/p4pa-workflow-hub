7a16c8d1efcbbdecdc23bc9039d2e08dead3c746
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-02-13 09:55:34 +0100

    P4ADEV-3749: added download and cache of SEND legal facts during SEND stream notification event.

--- src/main/java/it/gov/pagopa/pu/workflow/wf/pagopa/send/service/SendEventStreamProcessingServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/pagopa/send/service/SendEventStreamProcessingServiceImpl.java
@@ -1,7 +1,9 @@
 package it.gov.pagopa.pu.workflow.wf.pagopa.send.service;
 
+import it.gov.pagopa.payhub.activities.activity.sendnotification.FetchSendLegalFactActivity;
 import it.gov.pagopa.payhub.activities.activity.sendnotification.SendNotificationDateRetrieveActivity;
 import it.gov.pagopa.payhub.activities.activity.sendnotification.UpdateSendNotificationStatusActivity;
+import it.gov.pagopa.pu.sendnotification.dto.generated.LegalFactCategoryDTO;
 import it.gov.pagopa.pu.sendnotification.dto.generated.ProgressResponseElementV25DTO;
 import it.gov.pagopa.pu.sendnotification.dto.generated.SendNotificationDTO;
 import it.gov.pagopa.pu.workflow.dto.PaymentEventRequestDTO;
@@ -10,25 +12,29 @@ import it.gov.pagopa.pu.workflow.wf.pagopa.send.activity.PublishSendNotification
 import it.gov.pagopa.pu.workflow.wf.pagopa.send.mapper.SendNotification2DebtPositionSendNotificationsMapper;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+
 @Slf4j
 public class SendEventStreamProcessingServiceImpl implements SendEventStreamProcessingService {
 
   private final UpdateSendNotificationStatusActivity updateSendNotificationStatusActivity;
   private final SendNotificationDateRetrieveActivity sendNotificationDateRetrieveActivity;
   private final PublishSendNotificationPaymentEventActivity publishSendNotificationPaymentEventActivity;
+  private final FetchSendLegalFactActivity fetchSendLegalFactActivity;
 
   public SendEventStreamProcessingServiceImpl(
     UpdateSendNotificationStatusActivity updateSendNotificationStatusActivity,
     SendNotificationDateRetrieveActivity sendNotificationDateRetrieveActivity,
-    PublishSendNotificationPaymentEventActivity publishSendNotificationPaymentEventActivity) {
+    PublishSendNotificationPaymentEventActivity publishSendNotificationPaymentEventActivity, FetchSendLegalFactActivity fetchSendLegalFactActivity) {
     this.updateSendNotificationStatusActivity = updateSendNotificationStatusActivity;
     this.sendNotificationDateRetrieveActivity = sendNotificationDateRetrieveActivity;
     this.publishSendNotificationPaymentEventActivity = publishSendNotificationPaymentEventActivity;
+    this.fetchSendLegalFactActivity = fetchSendLegalFactActivity;
   }
 
   @Override
   public String processSendStreamEvent(String sendStreamId, ProgressResponseElementV25DTO streamEvent) {
-    return switch (streamEvent.getNewStatus()) {
+    String eventiId = switch (streamEvent.getNewStatus()) {
       case ACCEPTED -> {
         SendNotificationDTO sendNotification = this.updateSendNotificationStatusActivity.updateSendNotificationStatus(streamEvent.getNotificationRequestId());
         this.publishSendEvent(sendNotification, new PaymentEventRequestDTO(PaymentEventType.SEND_NOTIFICATION_CREATED, null));
@@ -52,6 +58,16 @@ public class SendEventStreamProcessingServiceImpl implements SendEventStreamProc
         yield null;
       }
     };
+    if(streamEvent.getElement().getLegalFactsIds() != null && !streamEvent.getElement().getLegalFactsIds().isEmpty()) {
+      streamEvent.getElement().getLegalFactsIds().forEach(lf -> {
+        try {
+          fetchSendLegalFactActivity.downloadAndCacheSendLegalFact(streamEvent.getNotificationRequestId(), LegalFactCategoryDTO.valueOf(lf.getCategory()), lf.getKey());
+        } catch (IOException e) {
+          log.error("Cannot download and cache legal fact for notification with notificationRequestId %s, category %s, legalFactId %s: %s".formatted(streamEvent.getNotificationRequestId(), lf.getCategory(), lf.getKey(), e.getMessage()), e);
+        }
+      });
+    }
+    return eventiId;
   }
 
   private void publishSendEvent(SendNotificationDTO sendNotificationDTO, PaymentEventRequestDTO eventRequestDTO) {
