77ab7d77dd88076219ae5d2a489745040e6509b1
michele.mazzini <michele.mazzini1996@gmail.com>
2026-03-17 12:49:41 +0100

    fix: increase code coverage

--- src/test/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/notice/wfmassivegeneration/MassiveNoticesGenerationWFImplTest.java
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/notice/wfmassivegeneration/MassiveNoticesGenerationWFImplTest.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.pu.workflow.wf.ingestionflow.notice.wfmassivegeneration;
 
+import io.temporal.workflow.Workflow;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.notice.FetchAndMergeNoticesActivity;
 import it.gov.pagopa.pu.workflow.wf.ingestionflow.notice.config.MassiveNoticesGenerationWFConfig;
 import org.junit.jupiter.api.AfterEach;
@@ -7,9 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+import java.time.Duration;
 
 @ExtendWith(MockitoExtension.class)
 class MassiveNoticesGenerationWFImplTest {
@@ -50,5 +55,44 @@ class MassiveNoticesGenerationWFImplTest {
     wf.generate(ingestionFlowFileId);
 
     Mockito.verify(fetchAndMergeNoticesActivityMock).fetchAndMergeNotices(ingestionFlowFileId);
+  }
+
+  @Test
+  void givenZeroResultWhenGenerateThenSleepAndRetry() {
+    Long ingestionFlowFileId = 1L;
+
+    Mockito.when(fetchAndMergeNoticesActivityMock.fetchAndMergeNotices(ingestionFlowFileId))
+      .thenReturn(0)
+      .thenReturn(0)
+      .thenReturn(1);
+
+    try (MockedStatic<Workflow> workflowMock = Mockito.mockStatic(Workflow.class)) {
+      workflowMock.when(() -> Workflow.sleep(Mockito.any(Duration.class))).then(invocation -> null);
+
+      wf.generate(ingestionFlowFileId);
+
+      Mockito.verify(fetchAndMergeNoticesActivityMock, Mockito.times(3)).fetchAndMergeNotices(ingestionFlowFileId);
+    }
+  }
+
+  @Test
+  void givenRepeatOneHundredTimesWhenGenerateThenContinueAsNew() {
+    Long ingestionFlowFileId = 1L;
+
+    Integer[] zeroResults = new Integer[99];
+    Arrays.fill(zeroResults, 0);
+
+    Mockito.when(fetchAndMergeNoticesActivityMock.fetchAndMergeNotices(ingestionFlowFileId))
+      .thenReturn(0, zeroResults)
+      .thenReturn(1);
+
+    try (MockedStatic<Workflow> workflowMock = Mockito.mockStatic(Workflow.class)) {
+      workflowMock.when(() -> Workflow.sleep(Mockito.any(Duration.class))).then(invocation -> null);
+
+      wf.generate(ingestionFlowFileId);
+
+      Mockito.verify(fetchAndMergeNoticesActivityMock, Mockito.times(101)).fetchAndMergeNotices(ingestionFlowFileId);
+      workflowMock.verify(() -> Workflow.continueAsNew(ingestionFlowFileId), Mockito.times(1));
+    }
   }
 }
