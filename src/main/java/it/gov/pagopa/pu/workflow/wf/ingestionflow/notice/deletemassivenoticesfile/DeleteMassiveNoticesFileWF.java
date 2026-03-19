f2b8b0c21fc5ef4d3e19bf75cc7f80d864138617
ElisKina-dev <elis.kina@nttdata.com>
2026-03-19 15:25:44 +0100

    fix

--- src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/notice/deletemassivenoticesfile/DeleteMassiveNoticesFileWF.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/notice/deletemassivenoticesfile/DeleteMassiveNoticesFileWF.java
@@ -3,6 +3,10 @@ package it.gov.pagopa.pu.workflow.wf.ingestionflow.notice.deletemassivenoticesfi
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 
+/**
+ * Link to Confluence page for DeleteMassiveNoticesFileWF
+ * * @see <a href=https://pagopa.atlassian.net/wiki/spaces/SPAC/pages/2780987489/Cancellazione+massiva+degli+avvisi>Confluence page</a>
+ */
 @WorkflowInterface
 public interface DeleteMassiveNoticesFileWF {
   @WorkflowMethod
--- src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/notice/deletemassivenoticesfile/DeleteMassiveNoticesFileWFImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/notice/deletemassivenoticesfile/DeleteMassiveNoticesFileWFImpl.java
@@ -4,10 +4,12 @@ import io.temporal.spring.boot.WorkflowImpl;
 import it.gov.pagopa.payhub.activities.activity.ingestionflow.notice.DeleteMassiveNoticesFileActivity;
 import it.gov.pagopa.pu.workflow.utilities.TaskQueueConstants;
 import it.gov.pagopa.pu.workflow.wf.ingestionflow.notice.config.DeleteMassiveNoticesFileWFConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+@Slf4j
 @WorkflowImpl(taskQueues = TaskQueueConstants.TASK_QUEUE_IMPORT_MEDIUM_PRIORITY)
 public class DeleteMassiveNoticesFileWFImpl implements DeleteMassiveNoticesFileWF, ApplicationContextAware {
 
@@ -21,6 +23,10 @@ public class DeleteMassiveNoticesFileWFImpl implements DeleteMassiveNoticesFileW
 
   @Override
   public void deleteMassiveNoticesFile(Long ingestionFlowFileId) {
+    log.info("START deleteMassiveNoticesFile for ingestionFlowFileId={}", ingestionFlowFileId);
+
     deleteMassiveNoticesFileActivity.deleteMassiveNoticesFile(ingestionFlowFileId);
+
+    log.info("END deleteMassiveNoticesFile for ingestionFlowFileId={}", ingestionFlowFileId);
   }
 }
