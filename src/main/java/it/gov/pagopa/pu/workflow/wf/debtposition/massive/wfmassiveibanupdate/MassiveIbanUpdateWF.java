741fd32ce3e68c3fc6d0d90644931bdd9a0053d1
antocalo <antonio.calo@nttdata.com>
2026-04-07 10:17:05 +0200

    P4ADEV-4536-implements-massiveIbanUpdate-wf

--- src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/massive/wfmassiveibanupdate/MassiveIbanUpdateWF.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/massive/wfmassiveibanupdate/MassiveIbanUpdateWF.java
@@ -2,7 +2,6 @@ package it.gov.pagopa.pu.workflow.wf.debtposition.massive.wfmassiveibanupdate;
 
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
-import it.gov.pagopa.pu.workflow.dto.generated.WorkflowCreatedDTO;
 
 /**
  * Workflow to update IBAN massively
