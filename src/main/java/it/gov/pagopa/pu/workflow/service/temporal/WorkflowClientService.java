2e9bbc47408f209b1024f32ca9456c38a3bf52d5
antocalo <antonio.calo@nttdata.com>
2026-04-07 09:09:47 +0200

    P4ADEV-4536-implements-massiveIbanUpdate-wf

--- src/main/java/it/gov/pagopa/pu/workflow/service/temporal/WorkflowClientService.java
+++ src/main/java/it/gov/pagopa/pu/workflow/service/temporal/WorkflowClientService.java
@@ -11,6 +11,7 @@ public interface WorkflowClientService {
   <A1, A2, A3> WorkflowCreatedDTO start(Functions.Proc3<A1, A2, A3> workflow, A1 arg1, A2 arg2, A3 arg3);
   <A1, A2, A3, A4> WorkflowCreatedDTO start(Functions.Proc4<A1, A2, A3, A4> workflow, A1 arg1, A2 arg2, A3 arg3, A4 arg4);
   <A1, A2, A3, A4, A5> WorkflowCreatedDTO start(Functions.Proc5<A1, A2, A3, A4, A5> workflow, A1 arg1, A2 arg2, A3 arg3, A4 arg4, A5 arg5);
+  <A1, A2, A3, A4, A5, A6> WorkflowCreatedDTO start(Functions.Proc6<A1, A2, A3, A4, A5, A6> workflow, A1 arg1, A2 arg2, A3 arg3, A4 arg4, A5 arg5, A6 arg6);
 
   WorkflowCreatedDTO signalWithStart(WorkflowStub workflowStub, String signalName, Object[] signalArgs, Object[] startArgs);
 }
