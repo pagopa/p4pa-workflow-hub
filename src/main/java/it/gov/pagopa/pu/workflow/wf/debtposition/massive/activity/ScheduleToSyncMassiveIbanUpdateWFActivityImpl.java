b4e1a6995d8234115cffec2e35f506390833c140
michele.mazzini <michele.mazzini1996@gmail.com>
2026-04-07 15:30:39 +0200

    feat: add local activity

---
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/massive/activity/ScheduleToSyncMassiveIbanUpdateWFActivityImpl.java
@@ -0,0 +1,46 @@
+package it.gov.pagopa.pu.workflow.wf.debtposition.massive.activity;
+
+import io.temporal.spring.boot.ActivityImpl;
+import it.gov.pagopa.pu.workflow.service.temporal.WorkflowClientService;
+import it.gov.pagopa.pu.workflow.service.temporal.WorkflowService;
+import it.gov.pagopa.pu.workflow.utilities.TaskQueueConstants;
+import it.gov.pagopa.pu.workflow.wf.debtposition.massive.wfmassiveibanupdate.MassiveIbanUpdateWF;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+import static it.gov.pagopa.pu.workflow.utilities.Utilities.generateWorkflowId;
+
+@Service
+@Slf4j
+@ActivityImpl(taskQueues = TaskQueueConstants.TASK_QUEUE_DP_LOW_PRIORITY_LOCAL)
+public class ScheduleToSyncMassiveIbanUpdateWFActivityImpl implements ScheduleToSyncMassiveIbanUpdateWFActivity {
+  private final WorkflowService workflowService;
+  private final WorkflowClientService workflowClientService;
+  private final Duration scheduleDuration;
+
+  public ScheduleToSyncMassiveIbanUpdateWFActivityImpl(
+    WorkflowService workflowService,
+    WorkflowClientService workflowClientService,
+    @Value("${workflow.massive-debt-position.schedule-minutes-massive-iban-update-to-sync}") int scheduleMinutes
+  ) {
+    this.workflowService = workflowService;
+    this.workflowClientService = workflowClientService;
+    this.scheduleDuration = Duration.ofMinutes(scheduleMinutes);
+  }
+
+  @Override
+  public void scheduleToSyncMassiveIbanUpdateWF(Long orgId, Long dptoId, String oldIban, String newIban, String oldPostalIban, String newPostalIban) {
+    log.info("Start of scheduling to sync massive iban update WF for org with id {}, with delay of {} minutes", orgId, this.scheduleDuration.toMinutes());
+    String workflowId = generateWorkflowId(orgId + "_TO_SYNC", MassiveIbanUpdateWF.class);
+    MassiveIbanUpdateWF workflow = workflowService.buildWorkflowStubDelayed(
+      MassiveIbanUpdateWF.class,
+      TaskQueueConstants.TASK_QUEUE_DP_LOW_PRIORITY,
+      workflowId,
+      this.scheduleDuration
+    );
+    workflowClientService.start(workflow::massiveIbanUpdate, orgId, dptoId, oldIban, newIban, oldPostalIban, newPostalIban);
+  }
+}
---
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/debtposition/massive/activity/ScheduleToSyncMassiveIbanUpdateWFActivityImplTest.java
@@ -0,0 +1,82 @@
+package it.gov.pagopa.pu.workflow.wf.debtposition.massive.activity;
+
+import it.gov.pagopa.pu.workflow.service.temporal.WorkflowClientService;
+import it.gov.pagopa.pu.workflow.service.temporal.WorkflowService;
+import it.gov.pagopa.pu.workflow.utilities.TaskQueueConstants;
+import it.gov.pagopa.pu.workflow.wf.debtposition.massive.wfmassiveibanupdate.MassiveIbanUpdateWF;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Duration;
+
+@ExtendWith(MockitoExtension.class)
+class ScheduleToSyncMassiveIbanUpdateWFActivityImplTest {
+  @Mock
+  private WorkflowService workflowServiceMock;
+  @Mock
+  private WorkflowClientService workflowClientServiceMock;
+  @Mock
+  private MassiveIbanUpdateWF massiveIbanUpdateWFMock;
+
+  private ScheduleToSyncMassiveIbanUpdateWFActivity scheduleToSyncMassiveIbanUpdateWFActivity;
+
+  private static final int SCHEDULE_MINUTES = 5;
+
+  @BeforeEach
+  void setUp() {
+    scheduleToSyncMassiveIbanUpdateWFActivity = new ScheduleToSyncMassiveIbanUpdateWFActivityImpl(
+      workflowServiceMock,
+      workflowClientServiceMock,
+      SCHEDULE_MINUTES
+    );
+  }
+
+  @AfterEach
+  void verifyNoMoreInteractions() {
+    Mockito.verifyNoMoreInteractions(workflowServiceMock, workflowClientServiceMock);
+  }
+
+  @Test
+  void whenScheduleToSyncMassiveIbanUpdateWFThenOk() {
+    Long orgId = 1L;
+    Long dptoId = 1L;
+    String oldIban = "oldIban";
+    String newIban = "newIban";
+    String oldPostalIban = "oldPostalIban";
+    String newPostalIban = "newPostalIban";
+
+    Duration scheduleDuration = Duration.ofMinutes(SCHEDULE_MINUTES);
+
+    Mockito.when(workflowServiceMock.buildWorkflowStubDelayed(
+        Mockito.eq(MassiveIbanUpdateWF.class),
+        Mockito.eq(TaskQueueConstants.TASK_QUEUE_DP_LOW_PRIORITY),
+        Mockito.anyString(),
+        Mockito.eq(scheduleDuration)))
+      .thenReturn(MassiveIbanUpdateWFMock);
+
+    scheduleToSyncMassiveIbanUpdateWFActivity.scheduleToSyncMassiveIbanUpdateWF(orgId, dptoId, oldIban, newIban, oldPostalIban, newPostalIban);
+
+    Mockito.verify(workflowServiceMock).buildWorkflowStubDelayed(
+      Mockito.eq(MassiveIbanUpdateWF.class),
+      Mockito.eq(TaskQueueConstants.TASK_QUEUE_DP_LOW_PRIORITY),
+      Mockito.anyString(),
+      Mockito.eq(retentionDuration)
+    );
+
+    Mockito.verify(workflowClientServiceMock)
+      .start(
+        Mockito.any(),
+        Mockito.eq(orgId),
+        Mockito.eq(dptoId),
+        Mockito.eq(oldIban),
+        Mockito.eq(newIban),
+        Mockito.eq(oldPostalIban),
+        Mockito.eq(newPostalIban)
+      );
+  }
+}
