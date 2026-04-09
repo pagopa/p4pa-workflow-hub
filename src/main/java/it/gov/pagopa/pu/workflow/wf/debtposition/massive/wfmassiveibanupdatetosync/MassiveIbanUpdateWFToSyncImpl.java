19224cc6b5ae6de5bc7a02e131f9143873b6a435
michele.mazzini <michele.mazzini1996@gmail.com>
2026-04-09 15:22:08 +0200

    fix

--- src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/massive/wfmassiveibanupdatetosync/MassiveIbanUpdateWFToSyncImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/massive/wfmassiveibanupdatetosync/MassiveIbanUpdateWFToSyncImpl.java
@@ -4,20 +4,31 @@ import io.temporal.spring.boot.WorkflowImpl;
 import io.temporal.workflow.Workflow;
 import it.gov.pagopa.payhub.activities.activity.debtposition.massive.MassiveIbanUpdateActivity;
 import it.gov.pagopa.pu.workflow.utilities.TaskQueueConstants;
+import it.gov.pagopa.pu.workflow.wf.debtposition.massive.config.MassiveDebtPositionWFConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 @Slf4j
 @WorkflowImpl(taskQueues = TaskQueueConstants.TASK_QUEUE_DP_LOW_PRIORITY)
-public class MassiveIbanUpdateWFToSyncImpl implements MassiveIbanUpdateWFToSync {
+public class MassiveIbanUpdateWFToSyncImpl implements MassiveIbanUpdateWFToSync, ApplicationContextAware {
   private MassiveIbanUpdateActivity massiveIbanUpdateActivity;
 
   private int loopExecutionCount = 0;
 
   private static final int WAITING_SECONDS_BEFORE_RETRY = 5 * 60;
   private static final int LOOP_EXECUTIONS_BEFORE_CLEAN_WF_HISTORY = 100;
+
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    MassiveDebtPositionWFConfig wfConfig = applicationContext.getBean(MassiveDebtPositionWFConfig.class);
+
+    massiveIbanUpdateActivity = wfConfig.buildMassiveIbanUpdateActivityStub();
+  }
 
   @Override
   public void massiveIbanUpdate(Long orgId, Long dptoId, String oldIban, String newIban, String oldPostalIban, String newPostalIban) {
