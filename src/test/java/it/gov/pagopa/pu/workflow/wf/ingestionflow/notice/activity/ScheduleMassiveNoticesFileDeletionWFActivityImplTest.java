50739e4e2b6e61a9f5617a22a2aa1d1179734ac1
michele.mazzini <michele.mazzini1996@gmail.com>
2026-04-23 10:50:50 +0200

    fix: remove semicolon

--- src/test/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/notice/activity/ScheduleMassiveNoticesFileDeletionWFActivityImplTest.java
+++ src/test/java/it/gov/pagopa/pu/workflow/wf/ingestionflow/notice/activity/ScheduleMassiveNoticesFileDeletionWFActivityImplTest.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 @ExtendWith(MockitoExtension.class)
 class ScheduleMassiveNoticesFileDeletionWFActivityImplTest {
   @Mock
-  private DeleteMassiveNoticesFileWFClient deleteMassiveNoticesFileWFClientMock;;
+  private DeleteMassiveNoticesFileWFClient deleteMassiveNoticesFileWFClientMock;
 
   private ScheduleMassiveNoticesFileDeletionWFActivity scheduleMassiveNoticesFileDeletionWFActivity;
 
