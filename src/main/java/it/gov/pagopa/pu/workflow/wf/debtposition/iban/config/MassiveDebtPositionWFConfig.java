3968d95e0c6996314376f22c193fc4d537c5e544
michele.mazzini <michele.mazzini1996@gmail.com>
2026-07-22 18:15:18 +0200

    fix: add heartbeat timeout to activity options

--- src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/iban/config/MassiveDebtPositionWFConfig.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/debtposition/iban/config/MassiveDebtPositionWFConfig.java
@@ -1,20 +1,32 @@
 package it.gov.pagopa.pu.workflow.wf.debtposition.iban.config;
 
+import io.temporal.activity.ActivityOptions;
 import io.temporal.workflow.Workflow;
 import it.gov.pagopa.payhub.activities.activity.debtposition.iban.MassiveIbanUpdateActivity;
 import it.gov.pagopa.pu.workflow.config.temporal.BaseWfConfig;
 import it.gov.pagopa.pu.workflow.config.temporal.TemporalWFImplementationCustomizer;
 import it.gov.pagopa.pu.workflow.utilities.TaskQueueConstants;
 import it.gov.pagopa.pu.workflow.wf.debtposition.iban.activity.ScheduleToSyncMassiveIbanUpdateWFActivity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
 @Configuration
 @ConfigurationProperties(prefix = "workflow.massive-debt-position")
 public class MassiveDebtPositionWFConfig extends BaseWfConfig {
+  private int heartbeatTimeoutInSeconds;
 
  public MassiveIbanUpdateActivity buildMassiveIbanUpdateActivityStub() {
-   return Workflow.newActivityStub(MassiveIbanUpdateActivity.class, TemporalWFImplementationCustomizer.baseWfConfig2ActivityOptions(this));
+   ActivityOptions activityOptions = ActivityOptions.newBuilder(TemporalWFImplementationCustomizer.baseWfConfig2ActivityOptions(this))
+     .setHeartbeatTimeout(Duration.ofSeconds(heartbeatTimeoutInSeconds))
+     .build();
+
+   return Workflow.newActivityStub(MassiveIbanUpdateActivity.class, activityOptions);
  }
 
   public ScheduleToSyncMassiveIbanUpdateWFActivity buildScheduleToSyncMassiveIbanUpdateWFActivityStub() {
