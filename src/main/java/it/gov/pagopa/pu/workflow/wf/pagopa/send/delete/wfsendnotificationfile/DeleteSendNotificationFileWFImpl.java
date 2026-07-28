d59c5b45f63505b4c5a81dddeaabc87dcb159930
Antonio Calò <antonio.calo@nttdata.com>
2026-07-28 13:39:22 +0200

    fix: P4ADEV-4863-change-task-queue-to-DeleteSendFileWF (#497)

--- src/main/java/it/gov/pagopa/pu/workflow/wf/pagopa/send/delete/wfsendnotificationfile/DeleteSendNotificationFileWFImpl.java
+++ src/main/java/it/gov/pagopa/pu/workflow/wf/pagopa/send/delete/wfsendnotificationfile/DeleteSendNotificationFileWFImpl.java
@@ -17,7 +17,7 @@ import java.time.Instant;
 import java.time.OffsetDateTime;
 
 @Slf4j
-@WorkflowImpl(taskQueues = TaskQueueConstants.TASK_QUEUE_LOW_PRIORITY)
+@WorkflowImpl(taskQueues = TaskQueueConstants.TASK_QUEUE_SEND_MEDIUM_PRIORITY)
 public class DeleteSendNotificationFileWFImpl implements DeleteSendNotificationFileWF, ApplicationContextAware {
   private DeleteSendNotificationFileActivity deleteSendNotificationFileActivity;
 
